6752a20471bf0693e6d35575bba0e1bd8235db21
devkavach22 <developer@kavachglobal.com>
2026-03-12 20:07:32 +0530

    Dockert Files Changes

--- vite.config.ts
+++ vite.config.ts
@@ -282,6 +282,6 @@ export default defineConfig({
     port: 3002,
     open: true,
     host: true,
-    allowedHosts: ["odoosaas.konverthr.com"],
+    allowedHosts: ["odoosaas.konverthr.com", "odooproduction.konverthr.com"],
   },
 });
